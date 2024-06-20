172f598deaedbf39e31b8872c8b6de9843073516
bklv.kirill <bklv.kirill@yandex.ru>
2024-06-20 23:29:46 +0500

    production

--- resources/views/components/headers/main.blade.php
+++ resources/views/components/headers/main.blade.php
@@ -1,4 +1,5 @@
-<header class="flex justify-between p-l-30 p-r-30 lt-lg:p-l-8 lt-lg:p-r-8 lt-sm:p-l-3 lt-sm:p-r-3 box-border items-center w-full h-[90px] bg-brand-bg color-brand-text">
+<header
+    class="flex justify-between p-l-30 p-r-30 lt-lg:p-l-8 lt-lg:p-r-8 lt-sm:p-l-3 lt-sm:p-r-3 box-border items-center w-full h-[90px] bg-brand-bg color-brand-text">
     <span class="font-bold text-[28px]">SibExpert</span>
     <nav>
         <ul id="navmenu" class="flex flex-row list-none gap-8 [&>*]:transition-all [&>*>*]:no-underline [&>*>*]:color-brand-text [&>*]:min-w-fit [&>*]:lt-sm:w-fit
@@ -17,7 +18,7 @@
             @endauth
         </ul>
     </nav>
-    <di class="flex gap-[24px] items-center" id="register-login">
+    <div class="flex gap-[24px] items-center" id="register-login">
         <ul class="flex list-none gap-8 [&>*>*]:no-underline [&>*>*]:color-brand-text lt-sm:hidden">
             @auth
                 <li class="sm:hidden"><a href="{{ route('user.log-out') }}">Выйти</a></li>
@@ -26,9 +27,11 @@
                 <li class="sm:hidden"><a href="{{ route('user.login') }}">Вход</a></li>
             @endauth
         </ul>
-        <button class="w-8 h-8 lg:hidden text-8 z-10 flex items-center justify-center bg-transparent border-none text-brand-text cursor-pointer" onclick="menuToggle()">
+        <button
+            class="w-8 h-8 lg:hidden text-8 z-10 flex items-center justify-center bg-transparent border-none text-brand-text cursor-pointer"
+            onclick="menuToggle()">
             <i class="ti ti-menu-2 active:scale-80 transform transition-all" id="burger-menu-button-open"></i>
             <i class="ti ti-x hidden active:scale-80 transform transition-all" id="burger-menu-button-close"></i>
         </button>
-        </div>
+    </div>
 </header>
